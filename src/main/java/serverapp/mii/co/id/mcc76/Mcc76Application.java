ba91fb776cc7fce3206d392726bad4398bcae19c
110 <hebertniconahason@gmail.com>
2023-03-31 11:09:23 +0700

    Generate project

---
+++ src/main/java/serverapp/mii/co/id/mcc76/Mcc76Application.java
@@ -0,0 +1,13 @@
+package serverapp.mii.co.id.mcc76;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mcc76Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mcc76Application.class, args);
+	}
+
+}
---
+++ src/test/java/serverapp/mii/co/id/mcc76/Mcc76ApplicationTests.java
@@ -0,0 +1,13 @@
+package serverapp.mii.co.id.mcc76;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mcc76ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
